61a29222b9e687f9e173ce4c5b665f360ed712da
Remitano <dev@remitano.com>
2021-05-12 12:16:20 +1000

    Rename script

--- git-push.sh
+++ git-push.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-# Credits: http://stackoverflow.com/a/750191
-
 git filter-branch -f --env-filter "
     GIT_AUTHOR_NAME='Remitano'
     GIT_AUTHOR_EMAIL='dev@remitano.com'
     GIT_COMMITTER_NAME='Remitano'
     GIT_COMMITTER_EMAIL='dev@remitano.com'
   " HEAD
+
+# Delete all other branches
+git branch | grep -v "master" | xargs git branch -D
+git push origin master -f
+git push proxy master -f
